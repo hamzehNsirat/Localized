6639582bd0b8faad4a8d92b90b9adb2123dd0e99
BmoxX <mohabuayyash2002@gmail.com>
2024-10-21 19:01:01 +0300

    'First Frontend commit, Routing semi-done'

---
+++ GradProj_2/Src/Front-End/src/App.jsx
@@ -0,0 +1,49 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import LandingPage from './components/Common/LandingPage.jsx';
+import Header from './components/Common/Header.jsx';
+import About from './components/Common/About.jsx';
+import ContactUs from './components/Common/ContactUs.jsx';
+import Login from './components/Login/Login.jsx';
+import SignUp from './components/SignUp/SignUp.jsx';
+import RetailerPage from './components/Retailer/RetailerPage.jsx';
+import RetailerHeader from './components/Retailer/Header.jsx';
+import SupplierPage from './components/Supplier/SupplierPage';
+import SupplierHeader from './components/Supplier/Header.jsx';
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+     <Routes>
+        <Route path='/' element={<Header />} >
+         <Route index element={<LandingPage/>}  />
+         <Route path="/about" element={<About />}/>
+         <Route path="/contactUs" element={<ContactUs />} />
+        </Route>
+
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signUp' element={<SignUp/>}/>
+
+        <Route path='/retailer' element={<RetailerHeader/>}>
+          <Route index element={<RetailerPage/>}/>
+          {/*<Route path='/marketplace' element={<RetailerMarketplace/>}/>*/}
+
+        </Route>
+
+        <Route path='/supplier' element={<SupplierHeader/>}>
+          <Route index element={<SupplierPage/>}/>
+        </Route>
+     </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
+
+
+
---
+++ GradProj_2/Src/Front-End/src/components/Supplier/ManagmentSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card, Button, ProgressBar } from 'react-bootstrap';
+
+const ManagementSection = ({ icon, title, description, buttonText, progress }) => {
+    return (
+        <Card className="mb-3 px-2 py-2" style={{ borderRadius: '10px',border:'0', backgroundColor:'#F2F2F2',boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.3)' }}>
+            <Card.Body className="d-flex justify-content-between align-items-center">
+                <div className="d-flex align-items-center">
+                    <div className="me-4" style={{ fontSize: '3rem' }}>
+                        {icon}
+                    </div>
+                    <div>
+                        <Card.Title className='fw-bold'>{title}</Card.Title>
+                        <Card.Text>{description}</Card.Text>
+
+                        {/* This should be integrated with the backend */}
+                        {progress && (
+                            <ProgressBar now={progress}  variant="danger" style={{ height: '5px' }} />
+                        )}
+                    </div>
+                </div>
+                <Button   className='rounded-pill px-5 py-2 d-flex align-content-center' style={{backgroundColor:'#BB4430',border:'none', boxShadow: '-4px 3px 1px rgba(0, 0, 0, 1)', fontSize:'14px'}}>{buttonText}</Button>
+
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default ManagementSection;
